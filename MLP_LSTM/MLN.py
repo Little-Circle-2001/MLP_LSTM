bc762aaabf8ce8bf4d5b18904b7da51d48723425
Little-Circle-2001 <83906459+Little-Circle-2001@users.noreply.github.com>
2025-03-26 10:28:26 +0800

    Add files via upload

---
+++ MLP_LSTM/MLN.py
@@ -0,0 +1,118 @@
+import torch
+from torch import nn
+
+class MLP_LSTM_Network(nn.Module):
+    def __init__(self, config):
+        super(MLP_LSTM_Network, self).__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.action_dim = config.action_dim
+        self.num_layers = config.num_layers
+        self.MLP_out = nn.Sequential(
+            nn.Linear(
+                in_features=2 * config.observe_dim + config.obstacle_dim * config.max_obstacle,
+                out_features=config.MLP_hidden_size
+            ),
+            nn.Linear(
+                in_features=config.MLP_hidden_size,
+                out_features=config.MLP_hidden_size
+            ),
+            nn.ReLU(),
+            nn.Linear(
+                in_features=config.MLP_hidden_size,
+                out_features=config.MLP_output_size
+            ),
+            nn.ReLU()
+        )
+
+        self.LSTM = nn.LSTM(
+            input_size=config.MLP_output_size + config.control_dim + 1,
+            hidden_size=config.hidden_size,
+            num_layers=config.num_layers,
+            batch_first=True
+        )
+
+        self.LSTM_out = nn.Linear(
+            in_features=config.hidden_size,
+            out_features=config.action_dim
+        )
+
+        # self.lstm = nn.LSTM(input_size=config.input_size+config.action_dim, hidden_size=config.hidden_size, num_layers=config.num_layers, batch_first=True)
+        # self.fc = nn.Linear(in_features=config.hidden_size, out_features=config.action_dim)
+
+    # def forward(self, observe, obstacle, mask):
+    def forward(self, observe, delta_observe_target, obstacle, mask, action, time):
+        batch_size, seq_len, observe_dim = observe.shape
+
+        multi_input = torch.cat((observe, delta_observe_target, (obstacle * mask.unsqueeze(-1)).view(batch_size, seq_len, -1)), dim=-1)
+        # [batch_size, seq_len, observe_dim+max_obstacle*obstacle_dim]
+
+        MLP_out = self.MLP_out(multi_input)       # [batch_size, seq_len, MLP_output_dim]
+
+        # MLP_out = []
+
+        # for t in range(seq_len):
+        #     x = multi_input[:, t, :]            # [batch_size, observe_dim+max_obstacle*obstacle_dim]
+        #     x = self.MLP_out(x)                 # [batch_size, MLP_output_dim]
+        #     MLP_out.append(x.unsqueeze(1))      # [batch_size, 1, MLP_action_dim]
+        # MLP_out = torch.cat(MLP_out, dim=1)     # [batch_size, seq_len, MLP_output_dim]
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)           # [batch_size, hidden_size]
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)           # [batch_size, hidden_size]
+
+        LSTM_input = torch.cat((MLP_out, action, time), dim=-1)     # [batch_size, seq_len, MLP_action_dim + action_dim + 1]
+        LSTM_out, (hn, cn)= self.LSTM(LSTM_input, (h0, c0))                 # [batch_size, sen_len, hidden_size]
+        output = self.LSTM_out(LSTM_out)                                    # [batch_size, seq_len, action_dim]
+
+        # output0 = torch.zeros(batch_size, self.action_dim)              # [batch_size, action_dim]
+        # output = torch.zeros(batch_size, seq_len, self.action_dim)
+        # for t in range(seq_len):
+        #     LSTM_input = torch.cat((MLP_out[:, t, :], output0), dim=-1)             # [batch_size, MLP_action_dim+action_dim]
+        #     LSTM_output, (hn, cn) = self.LSTM(LSTM_input.unsqueeze(1), (h0, c0))    # [batch_size, 1, hidden_size]
+        #     o = self.LSTM_out(LSTM_output)                                          # [batch_size, 1, action_dim]
+        #     h0 = hn
+        #     c0 = cn
+        #     output0 = o.squeeze(1)
+        #     output[:, t, :] = output0                                               # [batch_size, 1, action_dim]
+        return output
+
+
+# 配置文件类，传入超参数
+# class Config:
+#     def __init__(self):
+#         self.observe_dim = 6     # 输入特征的维度
+#         self.obstacle_dim = 4     # 障碍的维度
+#         self.max_obstacle = 5     # 最大允许观测到的障碍数量
+#         self.MLP_hidden_size = 128  # MLP 中间层的大小
+#         self.MLP_output_size = 2
+#         self.hidden_size = 64       # LSTM 隐藏层的大小
+#         self.num_layers = 1         # LSTM 层数
+#         self.action_dim = 3        # 输出的维度
+#         self.seq_len = 20           # 输入序列的长度
+#         self.action_tanh = False
+#
+# # 创建config实例
+# config = Config()
+
+# # 构建模型
+# model = MLP_LSTM_Network(config)
+#
+# # 示例输入数据，batch_size=32, seq_len=20, input_size=10
+# observe = torch.randn(32, 100, config.observe_dim)                              # [batch_size, seq_len, observe_dim]
+# action = torch.randn(32, 100, config.action_dim)
+#
+# obstacle = torch.randn(32, 100, config.max_obstacle, config.obstacle_dim)       # [batch_size, seq_len, max_obstacles, obstacle_dim]
+# mask = torch.randint(0, 2, (32, 100, config.max_obstacle)).float()              # [batch_size, seq_len, max_obstacles]，1表示有效，0表示无效
+# time = torch.randn(32, 100, 1)
+#
+#
+# # 前向传播
+# output = model(
+#     observe=observe,
+#     action=action,
+#     obstacle=obstacle,
+#     mask=mask,
+#     time=time
+# )
+
+# 输出预测结果
+# print(output)  # 输出的形状应为 [batch_size, seq_len, action_dim]
---
+++ MLP_LSTM/main_train.py
@@ -0,0 +1,349 @@
+import os
+import torch
+import argparse
+import sys
+
+root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_folder)
+
+from contourpy.util import data
+
+from MLP_LSTM.MLN import MLP_LSTM_Network
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+import numpy as np
+
+
+
+parser = argparse.ArgumentParser(description='transformer-rpod')
+parser.add_argument('--data_dir', type=str, default='dataset',
+                    help='defines directory from where to load files')
+args = parser.parse_args()
+args.data_dir = root_folder + '/' + args.data_dir
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Running on device: {device}\n")
+
+# Simulation configuration
+
+state_representation = 'rtn'
+
+# load the data
+print("Loading data...")
+
+if state_representation == 'roe':
+    torch_observe = torch.cat((torch.load(args.data_dir + '/torch_states_roe_scp.pth', weights_only=True), torch.load(args.data_dir + '/torch_states_roe_cvx.pth', weights_only=True)), dim=0)
+    torch_delta_observe_target = torch.cat((torch.load(args.data_dir + '/torch_delta_state_roe_cvx.pth', weights_only=True), torch.load(args.data_dir + '/torch_delta_state_roe_scp.pth', weights_only=True)), dim=0)
+else:
+    torch_observe = torch.cat((torch.load(args.data_dir + '/torch_states_rtn_scp.pth', weights_only=True), torch.load(args.data_dir + '/torch_states_rtn_cvx.pth', weights_only=True)), dim=0)
+    torch_delta_observe_target = torch.cat((torch.load(args.data_dir + '/torch_delta_state_rtn_cvx.pth', weights_only=True), torch.load(args.data_dir + '/torch_delta_state_rtn_scp.pth', weights_only=True)), dim=0)
+
+torch_obstacle = torch.cat((torch.load(args.data_dir + '/torch_delta_state_rtn_cvx_obs.pth', weights_only=True), torch.load(args.data_dir + '/torch_delta_state_rtn_scp_obs.pth', weights_only=True)), dim=0)
+torch_koz_vector = torch.cat((torch.load(args.data_dir + '/torch_koz_vector_cvx.pth', weights_only=True), torch.load(args.data_dir + '/torch_koz_vector_scp.pth', weights_only=True)), dim=0)
+torch_action = torch.cat((torch.load(args.data_dir + '/torch_actions_cvx.pth', weights_only=True), torch.load(args.data_dir + '/torch_actions_scp.pth', weights_only=True)), dim=0)
+torch_mask = torch.cat((torch.load(args.data_dir + '/torch_mask.pth', weights_only=True), torch.load(args.data_dir + '/torch_mask.pth', weights_only=True)), dim=0)
+
+data_size = torch_observe.shape[0]
+seq_len = torch_observe.shape[1]
+observe_dim = torch_observe.shape[-1]
+obstacle_dim = torch_obstacle.shape[-1] + 1
+max_num_obs = torch_obstacle.shape[2]
+action_dim = torch_action.shape[2]
+
+print('Completed\n')
+
+# 数据标准化
+observe_mean = torch_observe.mean(dim=0)
+observe_std = torch_observe.std(dim=0) + 1e-6
+
+delta_observe_target_mean = torch_delta_observe_target.mean(dim=0)
+delta_observe_target_std = torch_delta_observe_target.std(dim=0) + 1e-6
+
+obstacle_mean = torch_obstacle.mean(dim=0)
+obstacle_std = torch_obstacle.std(dim=0) + 1e-6
+
+action_mean = torch_action.mean(dim=0)
+action_std = torch_action.std(dim=0) + 1e-6
+
+observe_norm = (torch_observe - observe_mean) / (observe_std + 1e-6)
+delta_observe_target_norm = (torch_delta_observe_target - delta_observe_target_mean) / (delta_observe_target_std + 1e-6)
+delta_obstacle_norm = (torch_obstacle - obstacle_mean) / (obstacle_std + 1e-6)
+obstacle_norm = torch.cat((delta_obstacle_norm, torch_koz_vector), dim=-1)
+action_norm = (torch_action - action_mean) / (action_std + 1e-6)
+
+data_stats = {
+    'states_mean': observe_mean,
+    'states_std': observe_std,
+    'delta_states_target_mean': delta_observe_target_mean,
+    'delta_states_target_std': delta_observe_target_std,
+    'obstacle_mean': obstacle_mean,
+    'obstacle_std': obstacle_std,
+    'actions_mean': action_mean,
+    'actions_std': action_std,
+}
+
+np.savez_compressed(
+    '..\dataset\dataset-rpod-stats.npz',
+    states_std=data_stats['states_std'],
+    states_mean=data_stats['states_mean'],
+    delta_states_target_mean=data_stats['delta_states_target_mean'],
+    delta_states_target_std=data_stats['delta_states_target_std'],
+    obstacle_mean=data_stats['obstacle_mean'],
+    obstacle_std=data_stats['obstacle_std'],
+    actions_mean=data_stats['actions_mean'],
+    actions_std=data_stats['actions_std']
+)
+
+# Separate dataset in train and val data
+n_train = int(0.8 * data_size)
+n_eval = int(0.9 * data_size)
+
+train_data = {
+    'observes': observe_norm[:n_train, :, :],
+    'observes_target': delta_observe_target_norm[:n_train, :, :],
+    'obstacles': obstacle_norm[:n_train, :, :, :],
+    'masks': torch_mask[:n_train, :, :],
+    'actions': action_norm[:n_train, :, :]
+}
+eval_data = {
+    'observes': observe_norm[n_train:n_eval, :, :],
+    'observes_target': delta_observe_target_norm[n_train:n_eval, :, :],
+    'obstacles': obstacle_norm[n_train:n_eval, :, :, :],
+    'masks': torch_mask[n_train:n_eval, :, :],
+    'actions': action_norm[n_train:n_eval, :, :]
+}
+test_data = {
+    'observes': observe_norm[n_eval:, :, :],
+    'observes_target': delta_observe_target_norm[n_eval:, :, :],
+    'obstacles': obstacle_norm[n_eval:, :, :, :],
+    'masks': torch_mask[n_eval:, :, :],
+    'actions': action_norm[n_eval:, :, :]
+}
+
+class MLP_LSTM_Dataset(Dataset):
+    def __init__(self, split):
+        assert split in ['train', 'eval', 'test']
+        self.split = split
+        self.data = train_data if (split == 'train') else eval_data
+        self.data_size = self.data['observes'].shape[0]
+        self.seq_len = self.data['observes'].shape[1]
+
+    def __len__(self):
+        return self.data['observes'].shape[0]
+        # return len(self.data)
+
+    def __getitem__(self, idx):
+        # ix = torch.randint(0, self.data_size, (1,))
+        ix = idx
+        observes = self.data['observes'][ix, :, :].view(self.seq_len, observe_dim)
+        delta_observes_target = self.data['observes_target'][ix, :, :].view(self.seq_len, observe_dim)
+        obstacles = self.data['obstacles'][ix, :, :, :].view(self.seq_len, max_num_obs, obstacle_dim)
+        actions = self.data['actions'][ix, :, :].view(self.seq_len, action_dim)
+        mask = self.data['masks'][ix, :, :].view(self.seq_len, max_num_obs)
+        time = torch.tensor(range(self.seq_len)).view(self.seq_len, 1)
+        timesteps = (self.seq_len - 1 - time) / (self.seq_len - 1)
+
+        return observes, delta_observes_target, obstacles, actions, mask, timesteps, ix
+
+train_dataset = MLP_LSTM_Dataset(split='train')
+eval_dataset = MLP_LSTM_Dataset(split='eval')
+observes_i, delta_observes_target_i, obstacles_i, actions_i, mask_i, timesteps_i, ix = train_dataset[0]
+
+train_loader = DataLoader(
+    train_dataset,
+    # sampler=torch.utils.data.RandomSampler(
+    #     train_dataset, replacement=True, num_samples=int(6400)),
+    shuffle=True,
+    pin_memory=True,
+    batch_size=64,
+    num_workers=0,
+    drop_last=False
+)
+eval_loader = DataLoader(
+    eval_dataset,
+    shuffle=True,
+    pin_memory=True,
+    batch_size=64,
+    num_workers=0,
+    drop_last=False
+    )
+
+class Config:
+    def __init__(self):
+        self.observe_dim = observe_dim          # 输入特征的维度
+        self.obstacle_dim = obstacle_dim        # 障碍的维度
+        self.max_obstacle = max_num_obs         # 最大允许观测到的障碍数量
+        self.MLP_hidden_size = 128              # MLP 中间层的大小
+        self.MLP_output_size = 5                # MLP 输出层的大小
+        self.hidden_size = 128                  # LSTM 隐藏层的大小
+        self.num_layers = 2                     # LSTM 层数
+        self.action_dim = action_dim + observe_dim            # 输出的维度
+        self.control_dim = action_dim
+        self.seq_len = 30                       # 输入序列的长度
+        self.action_tanh = False
+
+# 创建config实例
+config = Config()
+
+model = MLP_LSTM_Network(config)
+optimizer = Adam(model.parameters(), lr=0.001)
+
+epochs = 1000
+eval_epoch = 50
+completed_step = 0
+
+def evaluate():
+    model.eval()
+    losses = []
+    losses_control = []
+    losses_state = []
+    for step, batch in enumerate(eval_loader):
+        # observes_i, actions_i, obstacles_i, mask_i, timesteps_i, ix = batch
+        observes_i, delta_observes_target_i, obstacles_i, actions_i, mask_i, _, ix = batch
+        batch_size = observes_i.shape[0]
+
+        time = torch.tensor(range(1 - config.seq_len, seq_len)).view(seq_len + config.seq_len - 1, 1)
+        timesteps_i = torch.empty(batch_size, seq_len + config.seq_len - 1, 1)
+        timesteps_i[:, :, :] = (seq_len - 1 - time) / (seq_len - 1)
+
+        observes_inputs = torch.empty((batch_size, config.seq_len, observe_dim))
+        delta_observes_target_inputs = torch.empty((batch_size, config.seq_len, observe_dim))
+        obstacles_inputs = torch.empty((batch_size, config.seq_len, config.max_obstacle, obstacle_dim))
+        mask_inputs = torch.empty((batch_size, config.seq_len, max_num_obs))
+        actions_inputs = torch.empty((batch_size, config.seq_len, action_dim))
+        timestep_inputs = torch.zeros((batch_size, config.seq_len, 1))
+
+        observes_inputs[:, :, :] = observes_i[:, [0], :]
+        delta_observes_target_inputs[:, :, :] = delta_observes_target_i[:, [0], :]
+        obstacles_inputs[:, :, :, :] = obstacles_i[:, [0], :, :]
+        mask_inputs[:, :, :] = mask_i[:, [0], :]
+        actions_inputs[:, :, :] = 0
+        timestep_inputs[:, :, :] = timesteps_i[:, :config.seq_len, :]
+
+        outputs = torch.zeros((batch_size, seq_len, config.action_dim))
+
+        for timestep in range(seq_len):
+            observes_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = observes_i[:, max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+            delta_observes_target_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = delta_observes_target_i[:, max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+            obstacles_inputs[:, max(-config.seq_len, -(timestep + 1)):, :, :] = obstacles_i[:, max(0, timestep - config.seq_len + 1):(timestep + 1), :, :]
+            mask_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = mask_i[:, max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+            timestep_inputs[:, :, :] = timesteps_i[:, timestep:timestep+config.seq_len, :]
+
+            output = model(
+                observe=observes_inputs,
+                delta_observe_target=delta_observes_target_inputs,
+                obstacle=obstacles_inputs,
+                mask=mask_inputs,
+                action=actions_inputs,
+                time=timestep_inputs
+            )
+            output = output[:, -1, :]
+            outputs[:, timestep, :] = output
+
+            actions_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = actions_i[:, max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+
+        control = outputs[:, :, :3]
+        state = outputs[:, :, 3:]
+        loss_i_control = torch.mean((control - actions_i) ** 2)
+        loss_i_state = torch.mean((state[:, :-1, :] - observes_i[:, 1:, :]) ** 2)
+        loss_i = loss_i_control + loss_i_state
+        losses_control.append(loss_i_control.item())
+        losses_state.append(loss_i_state.item())
+        losses.append(loss_i.item())
+    loss_control = torch.mean(torch.tensor(losses_control))
+    loss_state = torch.mean(torch.tensor(losses_state))
+    loss = torch.mean(torch.tensor(losses))
+    return loss_control.item(), loss_state.item(), loss.item()
+
+
+if __name__ == '__main__':
+    torch.autograd.set_detect_anomaly(True)
+    for epoch in range(epochs):
+        print('第{}轮训练开始，共{}轮'.format(epoch + 1, epochs))
+        for step, batch in enumerate(train_loader):
+            observes_i, delta_observes_target_i, obstacles_i, actions_i, mask_i, _, ix = batch
+            batch_size = observes_i.shape[0]
+
+            time = torch.tensor(range(1 - config.seq_len, seq_len)).view(seq_len + config.seq_len - 1, 1)
+            timesteps_i = torch.empty(batch_size, seq_len + config.seq_len - 1, 1)
+            timesteps_i[:, :, :] = (seq_len - 1 - time) / (seq_len - 1)
+
+            observes_inputs = torch.empty((batch_size, config.seq_len, observe_dim))
+            delta_observes_target_inputs = torch.empty((batch_size, config.seq_len, observe_dim))
+            obstacles_inputs = torch.empty((batch_size, config.seq_len, config.max_obstacle, obstacle_dim))
+            mask_inputs = torch.empty((batch_size, config.seq_len, max_num_obs))
+            actions_inputs = torch.empty((batch_size, config.seq_len, action_dim))
+            timestep_inputs = torch.zeros((batch_size, config.seq_len, 1))
+
+            observes_inputs[:, :, :] = observes_i[:, [0], :]
+            delta_observes_target_inputs[:, :, :] = delta_observes_target_i[:, [0], :]
+            obstacles_inputs[:, :, :, :] = obstacles_i[:, [0], :, :]
+            mask_inputs[:, :, :] = mask_i[:, [0], :]
+            actions_inputs[:, :, :] = 0
+            timestep_inputs[:, :, :] = timesteps_i[:, :config.seq_len, :]
+
+            outputs = torch.empty((batch_size, seq_len, config.action_dim))
+            masking_loss = torch.zeros(batch_size, config.seq_len, 1)
+
+            for timestep in range(seq_len):
+                observes_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = observes_i[:,max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+                delta_observes_target_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = delta_observes_target_i[:,max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+                obstacles_inputs[:, max(-config.seq_len, -(timestep + 1)):, :, :] = obstacles_i[:,max(0, timestep - config.seq_len + 1):(timestep + 1), :, :]
+                mask_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = mask_i[:,max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+                timestep_inputs[:, :, :] = timesteps_i[:, timestep:timestep+config.seq_len, :]
+
+                output = model(
+                    observe=observes_inputs,
+                    delta_observe_target=delta_observes_target_inputs,
+                    obstacle=obstacles_inputs,
+                    mask=mask_inputs,
+                    action=actions_inputs,
+                    time=timestep_inputs
+                )
+                # output = output[:, -1, :]
+
+                actions_inputs[:, max(-config.seq_len, -(timestep + 1)):, :] = actions_i[:, max(0, timestep - config.seq_len + 1):(timestep + 1), :]
+
+                control = output[:, :, :3]
+                state = output[:, :, 3:]
+                masking_loss[:, max(-config.seq_len, -(timestep + 1)):, :] = 1
+
+                loss_control = torch.mean(((control - actions_inputs) * masking_loss) ** 2)
+                loss_state = torch.mean(((state[:, :-1, :] - observes_inputs[:, 1:, :]) * masking_loss[:, 1:, :]) ** 2)
+                loss = loss_control + loss_state
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                completed_step = completed_step + 1
+
+                # if step % 10 == 0:
+                #     print(
+                #         f"Epoch {epoch + 1}/{epochs} ",
+                #         f"| Batch {step}/{len(train_loader)} ",
+                #         f"| Loss {loss.item()}"
+                #     )
+            # loss = torch.mean((outputs - actions_i) ** 2)
+            # optimizer.zero_grad()
+            # loss.backward(retain_graph=True)
+            # optimizer.step()
+            # completed_step = completed_step + 1
+            # completed_step += 1
+            if step % 10 == 0:
+                print(
+                    f"Epoch {epoch+1}/{epochs} ",
+                    f"| Batch {step}/{len(train_loader)} ",
+                    f"| Loss {loss.item()}"
+                )
+        if (epoch + 1) % (eval_epoch) == 0:
+            loss_control, loss_state, eval_loss = evaluate()
+            print(
+                f"control loss: {loss_control} ",
+                f"| state loss: {loss_state} ",
+                f"| eval loss: {eval_loss}"
+            )
+
+        if (epoch + 1) % eval_epoch*10 == 0:
+           torch.save(model.state_dict(), f=root_folder+'\MLP_LSTM\saved_files\checkpoints\checkpoint_MLP_LSTM_v6.pth')
+        print('第{}轮训练结束，共{}轮'.format(epoch + 1, epochs))
+
